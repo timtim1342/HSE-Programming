a6f363a8e07db3ca839d6445c2fa8b59053d7dd2
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2017-11-15 18:34:13 +0300

    Create homework4.py

---
+++ hw4/homework4.py
@@ -0,0 +1,18 @@
+tre=0
+uno=0
+with open("/home/uluru/Рабочий стол/OL2.txt") as f:
+    text=f.read()
+    text=text.replace('.','').replace(',','').replace('?','').replace('..','').replace('...','').replace('\ufeff','')
+    lis=list(text.split())
+for i in range(len(lis)):
+    if len(lis[i])==3:
+        tre+=1
+    elif len(lis[i])==1:
+        uno+=1
+print(tre,uno)
+if uno>tre:
+    print('oneletter words more than threeletters words')
+elif uno!=0:
+    print(tre/uno,'times more')
+else:
+    print('No oneletter words')
