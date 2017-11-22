203a78fdf294d6789a186a814de508338e9ae722
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2017-11-22 22:28:03 +0300

    Update homework4.py

--- hw4/homework4.py
+++ hw4/homework4.py
@@ -2,14 +2,13 @@ tre=0
 uno=0
 with open('OL2.txt', encoding='utf-8') as f:
     text=f.read()
-    text=text.replace('.','').replace(',','').replace('?','').replace('\ufeff','').replace('!','').replace(':','').replace(';','').replace('"','').replace('(','').replace(')','')
+    text=text.replace('.',' ').replace(',',' ').replace('?',' ').replace('\ufeff',' ').replace('!',' ').replace(':',' ').replace(';',' ').replace('"',' ').replace('(',' ').replace(')',' ')
     lis=list(text.split())
 for i in range(len(lis)):
     if len(lis[i])==3:
         tre+=1
     elif len(lis[i])==1:
         uno+=1
-print(tre,uno)
 if uno>tre:
     print('oneletter words more than threeletters words')
 elif uno!=0:
