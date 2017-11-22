75d26dd3e37f58a3849327121b90521104f3ad12
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2017-11-22 22:32:45 +0300

    Update homework4.py

--- hw4/homework4.py
+++ hw4/homework4.py
@@ -1,17 +1,17 @@
-tre=0
-uno=0
-with open('OL2.txt', encoding='utf-8') as f:
-    text=f.read()
-    text=text.replace('.',' ').replace(',',' ').replace('?',' ').replace('\ufeff',' ').replace('!',' ').replace(':',' ').replace(';',' ').replace('"',' ').replace('(',' ').replace(')',' ')
-    lis=list(text.split())
+tre = 0
+uno = 0
+with open('OL2.txt', encoding = 'utf-8') as f:
+    text = f.read()
+    text = text.replace('.',' ').replace(',',' ').replace('?',' ').replace('\ufeff',' ').replace('!',' ').replace(':',' ').replace(';',' ').replace('"',' ').replace('(',' ').replace(')',' ')
+    lis = list(text.split())
 for i in range(len(lis)):
-    if len(lis[i])==3:
-        tre+=1
-    elif len(lis[i])==1:
-        uno+=1
-if uno>tre:
+    if len(lis[i]) == 3:
+        tre += 1
+    elif len(lis[i]) == 1:
+        uno += 1
+if uno > tre:
     print('oneletter words more than threeletters words')
-elif uno!=0:
+elif uno != 0:
     print(tre/uno,'times more')
 else:
     print('No oneletter words')
