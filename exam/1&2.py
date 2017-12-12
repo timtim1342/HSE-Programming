49d0151fe0dd886f934945a5132f1214e532eee8
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2017-12-12 14:30:53 +0300

    Update 1&2.py

--- exam/1&2.py
+++ exam/1&2.py
@@ -1,1 +1,8 @@
-f
+count=0
+with open("Extinct_languages.tsv", encoding="utf-8") as f:
+    for line in f:
+        if len(line)>35:
+            print(line)
+        if 'Critically endangered' in line:
+            count+=1
+print('Число языков critically endangered:',count)
