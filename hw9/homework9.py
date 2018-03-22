449646f3561a24e2f486b27ee4baef40832465fb
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2018-03-22 20:16:46 +0300

    Update homework9.py

--- hw9/homework9.py
+++ hw9/homework9.py
@@ -2,7 +2,7 @@ import re
 lis=[]
 with open('search.txt', encoding='utf-8') as f:
     text = f.read().lower()
-    all_res = re.findall('программир(?:ова|у)[а-я][^и][а-я]*', text)
+    all_res = re.findall('программиру[а-я]{1,5}|программированн[а-я]{1,3}|программирова[^н\s]{1,4}', text)
 for i in range(len(all_res)):
     if all_res[i] not in lis:
         lis.append(all_res[i])
