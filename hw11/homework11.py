24b5722e22833e77c33ab37d63b9d0e9787ccc85
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2018-04-08 16:12:01 +0300

    Create homework11.py

---
+++ hw11/homework11.py
@@ -0,0 +1,15 @@
+import re
+def opn():
+    with open('Лингвистика — Википедия.txt', encoding='utf-8') as f:
+        text = f.read()
+    return text
+            
+def shashlik_mashlik():
+    txt = 'язык языкознание дом языку плов языках языки'# не меняет если языки идут подряд
+    return re.sub(r'\bЯзык([иауео]?[хмв]?и?)\b', r'Шашлык\1', re.sub(r'\bязык([иауео]?[хмв]?и?)\b', r'шашлык\1', opn()))
+
+def make_shashlik():
+    with open('shashlik.txt', 'w', encoding='utf-8') as f:
+        f.write(shashlik_mashlik())
+if __name__ == '__main__':
+    make_shashlik()
