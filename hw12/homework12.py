3d53db106e72d65baf87007d6aabec58e9aca833
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2018-04-17 19:23:21 +0300

    Create homework12.py

---
+++ hw12/homework12.py
@@ -0,0 +1,19 @@
+import os, re
+
+def create_list_dir():
+    dir_list = str(os.listdir())
+    return dir_list
+
+def find_good_ones():
+    res = re.findall(r'\'\b([а-яА-Я]+)\b\'', create_list_dir())
+    return res
+
+def our_achievements():
+    ress = find_good_ones()
+    print('Всего папок, сожержащих только кириллические символы, вот столько:', len(ress))
+    print('Такие вот папки:')
+    for i in range(len(ress)):
+        print(ress[i])
+
+if __name__ == '__main__':
+    our_achievements()
